1e0258abac71ad2b305fde6b559705e9bbc2e9a3
laylla986 <lbss7@aluno.ifal.edu.br>
2024-12-08 19:58:04 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+let soma = 0;
+
+for (let i = 1; i <= 20; i++) {
+
+    let idade = ('Digite a idade da ' + i + ' pessoa:'));
+    
+    soma += idade;
+}
+let media = somaidades/20;
+
+alert('A soma das idades é: ' + soma);
